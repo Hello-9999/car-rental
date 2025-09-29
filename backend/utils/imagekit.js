86585909cba22767c847b9c9a2fa0b1128a1c07c
Gautam Magar <116451474+Hello-9999@users.noreply.github.com>
2025-09-29 19:57:44 +0545

    Refactor ImageKit configuration with empty
    Updated ImageKit initialization to use empty keys.

--- backend/utils/imagekit.js
+++ backend/utils/imagekit.js
@@ -3,9 +3,10 @@ import ImageKit from "imagekit";
 // or
 
 var imagekit = new ImageKit({
-  publicKey: "public_ahpa872+t+HinrhN9tzwJ9o13VE=",
-  privateKey: "private_zjmQboO6TRHXlyq4UePJ0d6p2EM=",
-  urlEndpoint: "https://ik.imagekit.io/5raqegdne",
+  publicKey: '',
+  privateKey:  '',
+  urlEndpoint:  ''
 });
 
 export default imagekit;
+
